a3b162de1c7c626a4fccb2e26cb8e21487d66019
Ryu JuHeon <saidbysolo@gmail.com>
2021-06-09 00:07:45 +0900

    refactor!(auto_complete): using api

--- script/auto_complete.py
+++ script/auto_complete.py
@@ -1,11 +1,8 @@
-import json
+from json import dumps, loads
 import logging
-import re
-from time import sleep
-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.remote.webelement import WebElement
+from aiohttp import ClientSession
+from asyncio.runners import run
+from asyncio.tasks import sleep
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
@@ -21,57 +18,77 @@ fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 
 
-logger.addHandler(fh)
-logger.addHandler(ch)
+class Request:
+    crawled_dict = {}
 
-logger.info("Starting")
+    def __init__(self, key) -> None:
+        self.key = key
+        self.session = None
+        self._req_list = []
 
-driver = webdriver.Chrome("./chromedriver.exe")
+    def load_character(self):
+        logger.info("load character file")
+        with open("result-korean-character.json", "r", encoding="UTF-8") as f:
+            return loads(f.read())
+        
+    
+    async def request(self, query):
+        PARAMS = {
+            "limit": 1,
+            "indent": "true",
+            "languages": "ko",
+            "key":self.key,
+            "query": query
+        }
+        if not self.session:
+            logger.info("Make Session")
+            self.session = ClientSession()
+        
+        for _ in range(5):
+            async with self.session.get("https://kgsearch.googleapis.com/v1/entities:search", params=PARAMS) as response:
+                logger.info("get %s", query)
+                if response.status == 429:
+                    logger.warn("429 sleep")
+                    await sleep(30)
+                    continue
 
-crawled_dict = {}
+                if element := (await response.json())["itemListElement"]:
+                    result = element[0]["result"]
+                    if (desc := result.get("description")) and "등장" in desc:
+                        logger.info("found korean_name: %s",  result["name"])
+                        return {query: result["name"]}
+                logger.info("Not found %s", query)
+                return {query: ""}
 
-with open("result-korean-character.json", "r", encoding="UTF-8") as f:
-    charactors_json = json.loads(f.read())
-    logger.info("Complete reading file")
-
-try:
-    for charactor_key in charactors_json.keys():
-        logger.info("%s", charactor_key)
-        driver.get(f"https://www.google.com/search?q={charactor_key}")
-        logger.info("Get url: %s", f"https://www.google.com/search?q={charactor_key}")
+    
+    async def __run(self):
         try:
-            main_element: WebElement = driver.find_element_by_class_name("main")
-            logger.info("found main element")
-        except NoSuchElementException:
-            logger.warning("Detect capcha sleep 60 sec")
-            sleep(60)
-            logger.info("retry")
-            driver.get(f"https://www.google.com/search?q={charactor_key}")
-            logger.info(
-                "Get url: %s", f"https://www.google.com/search?q={charactor_key}"
-            )
-            main_element: WebElement = driver.find_element_by_class_name("main")
-        else:
-            try:
-                charactor_element: WebElement = main_element.find_element_by_xpath(
-                    '//*[@data-attrid="title"]'
-                )
-                logger.info("found charactor element")
-            except NoSuchElementException:
-                logger.info("Can't found korean_name: %s", charactor_key)
-                continue
-            else:
-                regex_pattern = r"(.+)?(?(1) \(.+\)|.+)"
+            logger.info("start append task")
+            for charactor_key in self.load_character():
+                self._req_list.append(self.request(charactor_key))
+            logger.info("Complete append task")
 
-                parsed = re.match(regex_pattern, charactor_element.text)
-                if regex_groups := parsed.groups()[0]:
-                    korean_name = regex_groups
-                    logger.info("found in group 1")
-                else:
-                    korean_name = parsed.group()
-                    logger.info("found in match 1")
-                logger.info("found korean_name: %s", korean_name)
-                crawled_dict[charactor_key] = korean_name
-except Exception:
-    with open("crawled.json", "w") as f:
-        f.write(json.dumps(crawled_dict))
+            logger.info("Start Tasks")
+            # TODO: more faster
+            count = 0
+            total = len(self._req_list)
+            for req in self._req_list:
+                count += 1
+                logger.info("%s/%s", count, total)
+                self.crawled_dict.update(await req)
+        finally:
+            logger.info("close clientsession")
+            await self.session.close()
+
+            with open("acrawled.json", "w") as f:
+                logger.info("Saving...")
+                f.write(dumps(self.crawled_dict))
+                logger.info("Done!")
+
+    def run(self):
+        logger.info("Starting")
+        run(self.__run())
+        logger.info("Done.")
+
+
+Request("api key here").run()
